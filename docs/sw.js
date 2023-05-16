496cc2926063183b7a2084b354d455ba361bad47
shiro46mt <39456646+shiro46mt@users.noreply.github.com>
2023-05-17 00:05:41 +0900

    add service worker

---
+++ docs/sw.js
@@ -0,0 +1,34 @@
+var CACHE_NAME = '10puzzle';
+var urlsToCache = [
+    'index.html',
+    'hard/index.html',
+    'style.css',
+    'main.css',
+    'js/style.js',
+    'js/main.js',
+    'js/moment.js',
+    'js/moment-timezone.js',
+    'js/data.json',
+];
+
+// インストール処理
+self.addEventListener('install', function(event) {
+    event.waitUntil(
+        caches
+            .open(CACHE_NAME)
+            .then(function(cache) {
+                return cache.addAll(urlsToCache);
+            })
+    );
+});
+
+// リソースフェッチ時のキャッシュロード処理
+self.addEventListener('fetch', function(event) {
+    event.respondWith(
+        caches
+            .match(event.request)
+            .then(function(response) {
+                return response || fetch(event.request);
+            })
+    );
+});
